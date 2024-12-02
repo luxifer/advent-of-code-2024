ec9346f80770bed817059232b05b2f97abcd8db5
Florent Viel <fviel@scaleway.com>
2024-12-02 13:29:10 +0100

    answer day two

---
+++ day-02/src/main.rs
@@ -0,0 +1,97 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let input = std::env::args().nth(1).expect("missing input");
+
+    let mut reports: Vec<Vec<i32>> = Vec::new();
+
+    if let Ok(lines) = read_lines(input) {
+        for line in lines.flatten() {
+            let parts: Vec<&str> = line.split(" ").collect();
+            let levels: Vec<i32> = parts.iter().map(|p| p.parse::<i32>().unwrap()).collect();
+            reports.push(levels);
+        }
+    }
+
+    let mut safe: i32 = 0;
+
+    for levels in reports.iter() {
+        if is_safe(levels.clone()) {
+            safe += 1;
+        }
+    }
+
+    println!("answer: {}", safe);
+
+    let mut safe: i32 = 0;
+
+    for levels in reports.iter() {
+        let mut any_safe = false;
+
+        for i in 0..levels.len() {
+            let mut fixed: Vec<i32> = levels.to_vec();
+            fixed.remove(i);
+
+            if is_safe(fixed) {
+                any_safe = true;
+                break;
+            }
+        }
+
+        if any_safe {
+            safe += 1;
+        }
+    }
+
+    println!("answer: {}", safe);
+}
+
+fn is_safe(levels: Vec<i32>) -> bool {
+    let mut prev: i32 = -1;
+    let mut is_safe: bool = true;
+    let mut dir = 0;
+
+    for level in levels.iter() {
+        let mut new_dir = 0;
+        if prev != -1 {
+            let diff = prev.abs_diff(*level);
+            if diff > 3 {
+                is_safe = false;
+                break;
+            }
+
+            if prev == *level {
+                is_safe = false;
+                break;
+            }
+
+            if prev < *level {
+                new_dir = 1;
+            } else {
+                new_dir = -1
+            }
+        }
+
+        if dir != 0 {
+            if dir != new_dir {
+                is_safe = false;
+                break;
+            }
+        }
+
+        prev = *level;
+        dir = new_dir;
+    }
+
+    return is_safe;
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
