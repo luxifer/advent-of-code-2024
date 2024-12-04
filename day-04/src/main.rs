ac14d4efbe8682a746505ae5a9d3ee2a5de3214a
luxifer <luxifer666@gmail.com>
2024-12-04 22:13:37 +0100

    fix day four

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -9,24 +10,26 @@ fn main() {
     let mut word_search: Matrix<char> = Matrix::new();
 
     if let Ok(lines) = read_lines(input) {
-        let mut cols = 0;
+        let mut cols = -1;
         let mut rows = 0;
         for line in lines.flatten() {
-            cols = line.len() as i32;
-            rows += 1;
+            if cols == -1 {
+                cols = line.len() as i32;
+            }
             for char in line.chars() {
                 word_search.data.push(char);
             }
+            rows += 1;
         }
-        word_search.len.x = cols;
-        word_search.len.y = rows;
+        word_search.width = cols;
+        word_search.height = rows;
     }
-
-    println!("{}", word_search);
 
     let mut total = 0;
 
     for c in word_search.iter() {
+        // println!("{}", c);
+
         let val = word_search.at_coord(c).unwrap();
 
         if *val != 'X' {
@@ -45,17 +48,37 @@ fn main() {
     }
 
     println!("answer: {}", total);
+
+    let mut total = 0;
+
+    for c in word_search.iter() {
+        // println!("{}", c);
+
+        let val = word_search.at_coord(c).unwrap();
+
+        if *val != 'A' {
+            continue;
+        }
+
+        if search_cross_mas(&word_search, c) {
+            total += 1;
+        }
+    }
+
+    println!("answer: {}", total);
 }
 
-fn search_xmas(m: &Matrix<char>, orig: Coord, dir: Dir) -> bool {
+fn search_xmas(m: &Matrix<char>, orig: Coord, dir: Vector) -> bool {
     let word = ['X', 'M', 'A', 'S'];
 
     for i in 0..word.len() {
         let curr = orig.add(dir.mul(i as i32));
-        let val = m.at_coord(curr);
-        if val.is_none() {
+
+        if !m.in_coord(curr) {
             return false;
         }
+
+        let val = m.at_coord(curr);
 
         if *val.unwrap() != word[i] {
             return false;
@@ -63,6 +86,35 @@ fn search_xmas(m: &Matrix<char>, orig: Coord, dir: Dir) -> bool {
     }
 
     // println!("found at {} -> {}", orig, dir);
+
+    return true;
+}
+
+fn search_cross_mas(m: &Matrix<char>, orig: Coord) -> bool {
+    let directions = [UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT];
+
+    let mut xs = HashMap::from([('M', 0), ('S', 0)]);
+    for v in directions {
+        let curr = orig.add(v);
+        if !m.in_coord(curr) {
+            return false;
+        }
+
+        let val = m.at_coord(curr);
+        xs.entry(*val.unwrap()).and_modify(|v| *v += 1);
+    }
+
+    if *xs.get(&'M').unwrap() != 2 || *xs.get(&'S').unwrap() != 2 {
+        return false;
+    }
+
+    let ul = m.at_coord(orig.add(UP_LEFT));
+    let dr = m.at_coord(orig.add(DOWN_RIGHT));
+
+    // check for MAM or SAS
+    if ul.unwrap() == dr.unwrap() {
+        return false;
+    }
 
     return true;
 }
@@ -77,23 +129,26 @@ where
 
 struct Matrix<T> {
     data: Vec<T>,
-    len: Coord,
+    width: i32,
+    height: i32,
 }
 
 impl<T: std::clone::Clone> Matrix<T> {
     fn new() -> Matrix<T> {
         return Matrix {
             data: Vec::new(),
-            len: Coord::new(),
+            width: 0,
+            height: 0,
         };
     }
 
     fn at_coord(&self, c: Coord) -> Option<&T> {
-        let pos = c.y * self.len.x + c.x;
-        if pos < 0 || pos > self.len.x * self.len.y {
-            return None;
-        }
+        let pos = c.y * self.width + c.x;
         return self.data.get(pos as usize);
+    }
+
+    fn in_coord(&self, c: Coord) -> bool {
+        return c.x >= 0 && c.x < self.width && c.y >= 0 && c.y < self.height;
     }
 
     fn iter(&self) -> MatrixIterator<T> {
@@ -106,7 +161,7 @@ impl<T: std::clone::Clone> Matrix<T> {
 
 impl<T> fmt::Display for Matrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "size: {}x{}", self.len.x, self.len.y)
+        write!(f, "size: {}x{}", self.width, self.height)
     }
 }
 
@@ -124,12 +179,14 @@ impl<'a, T> Iterator for MatrixIterator<'a, T> {
             return None;
         }
 
-        let i = self.pos_iter as i32;
-        self.pos_iter += 1;
-        let x = i % self.matrix.len.x;
-        let y = i / self.matrix.len.x;
+        let c = Coord {
+            x: self.pos_iter as i32 % self.matrix.width,
+            y: self.pos_iter as i32 / self.matrix.width,
+        };
 
-        return Some(Coord { x, y });
+        self.pos_iter += 1;
+
+        return Some(c);
     }
 }
 
@@ -144,10 +201,10 @@ impl Coord {
         return Coord { x: 0, y: 0 };
     }
 
-    fn add(&self, d: Dir) -> Coord {
+    fn add(&self, v: Vector) -> Coord {
         return Coord {
-            x: self.x + d.x,
-            y: self.y + d.y,
+            x: self.x + v.x,
+            y: self.y + v.y,
         };
     }
 }
@@ -159,38 +216,38 @@ impl fmt::Display for Coord {
 }
 
 #[derive(Copy, Clone)]
-struct Dir {
+struct Vector {
     x: i32,
     y: i32,
 }
 
-impl Dir {
-    const fn add(&self, d: Dir) -> Dir {
-        return Dir {
-            x: self.x + d.x,
-            y: self.y + d.y,
+impl Vector {
+    const fn add(&self, v: Vector) -> Vector {
+        return Vector {
+            x: self.x + v.x,
+            y: self.y + v.y,
         };
     }
 
-    fn mul(&self, n: i32) -> Dir {
-        return Dir {
+    fn mul(&self, n: i32) -> Vector {
+        return Vector {
             x: self.x * n,
             y: self.y * n,
         };
     }
 }
 
-impl fmt::Display for Dir {
+impl fmt::Display for Vector {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
 
-const LEFT: Dir = Dir { x: -1, y: 0 };
-const RIGHT: Dir = Dir { x: 1, y: 0 };
-const UP: Dir = Dir { x: 0, y: 1 };
-const DOWN: Dir = Dir { x: 0, y: -1 };
-const UP_LEFT: Dir = UP.add(LEFT);
-const UP_RIGHT: Dir = UP.add(RIGHT);
-const DOWN_LEFT: Dir = DOWN.add(LEFT);
-const DOWN_RIGHT: Dir = DOWN.add(RIGHT);
+const LEFT: Vector = Vector { x: -1, y: 0 };
+const RIGHT: Vector = Vector { x: 1, y: 0 };
+const UP: Vector = Vector { x: 0, y: -1 };
+const DOWN: Vector = Vector { x: 0, y: 1 };
+const UP_LEFT: Vector = UP.add(LEFT);
+const UP_RIGHT: Vector = UP.add(RIGHT);
+const DOWN_LEFT: Vector = DOWN.add(LEFT);
+const DOWN_RIGHT: Vector = DOWN.add(RIGHT);
