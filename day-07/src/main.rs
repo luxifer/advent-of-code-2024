4534ccb693383aae37aab193a445b2e620c7028c
luxifer <luxifer666@gmail.com>
2024-12-07 16:16:38 +0100

    answer day seven

---
+++ day-07/src/main.rs
@@ -0,0 +1,136 @@
+use std::fmt;
+use std::fmt::Write;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use itertools::{repeat_n, Itertools};
+
+fn main() {
+    let input = std::env::args().nth(1).expect("missing input");
+
+    let mut equations: Vec<Equation> = Vec::new();
+
+    if let Ok(lines) = read_lines(input) {
+        for line in lines.flatten() {
+            let parts: Vec<&str> = line.split(": ").collect();
+            let test_value = parts[0].parse::<u64>().unwrap();
+            let numbers: Vec<u64> = parts[1]
+                .split(" ")
+                .map(|p| p.parse::<u64>().unwrap())
+                .collect();
+
+            equations.push(Equation::from(test_value, numbers));
+        }
+    }
+
+    let mut total = 0;
+
+    for equation in equations.iter() {
+        if compute_combination(
+            equation.test_value,
+            equation.numbers.clone(),
+            vec![Operator::Addition, Operator::Multiplication],
+        ) {
+            total += equation.test_value;
+        }
+    }
+
+    println!("answer: {}", total);
+    total = 0;
+
+    for equation in equations.iter() {
+        if compute_combination(
+            equation.test_value,
+            equation.numbers.clone(),
+            vec![
+                Operator::Addition,
+                Operator::Multiplication,
+                Operator::Concatenation,
+            ],
+        ) {
+            total += equation.test_value;
+        }
+    }
+
+    println!("answer: {}", total);
+}
+
+struct Equation {
+    test_value: u64,
+    numbers: Vec<u64>,
+}
+
+impl Equation {
+    fn from(test_value: u64, numbers: Vec<u64>) -> Equation {
+        Equation {
+            test_value,
+            numbers,
+        }
+    }
+}
+
+fn compute_combination(expected: u64, inputs: Vec<u64>, operators: Vec<Operator>) -> bool {
+    let permutations: Vec<_> = repeat_n(operators.clone(), inputs.len() - 1)
+        .multi_cartesian_product()
+        .collect();
+
+    for permutation in permutations.iter() {
+        let mut total = inputs[0];
+        let mut buf = String::new();
+        write!(buf, "{}: {}", expected, inputs[0]).unwrap();
+
+        for i in 0..permutation.len() {
+            write!(buf, " {} {}", permutation[i], inputs[i + 1]).unwrap();
+
+            if permutation[i] == Operator::Addition {
+                total += inputs[i + 1];
+            }
+
+            if permutation[i] == Operator::Multiplication {
+                total *= inputs[i + 1];
+            }
+
+            if permutation[i] == Operator::Concatenation {
+                let concatenation = total.to_string() + &inputs[i + 1].to_string();
+                total = concatenation.parse::<u64>().unwrap();
+            }
+
+            if total > expected {
+                break;
+            }
+        }
+
+        if total == expected {
+            // println!("{} = {}", buf, total);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Clone, PartialEq, Debug)]
+enum Operator {
+    Addition,
+    Multiplication,
+    Concatenation,
+}
+
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Operator::Addition => write!(f, "+"),
+            Operator::Multiplication => write!(f, "*"),
+            Operator::Concatenation => write!(f, "||"),
+        }
+    }
+}
