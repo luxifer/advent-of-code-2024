e0dd2ea5707d5ab8bec0919ea60c65821f7bec7c
Florent Viel <fviel@scaleway.com>
2025-12-08 10:51:33 +0100

    complete day 6

--- 2025/day-06/main.rs
+++ 2025/day-06/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use common::cli;
-use ndarray::{Array, Array2, Axis};
 
 fn main() -> Result<()> {
     let app = cli::Cli::new();
@@ -9,9 +8,11 @@ fn main() -> Result<()> {
     let mut numbers: Vec<Vec<i64>> = Vec::new();
     let mut operators: Vec<Operator> = Vec::new();
     let mut problem_len = 0;
+    let mut inputs: Vec<Vec<char>> = Vec::new();
 
     for line in lines.lines() {
         problem_len = line.len();
+        inputs.push(line.chars().collect());
         let parts: Vec<&str> = line.split_whitespace().collect();
 
         if parts[0] == "+" || parts[0] == "*" {
@@ -44,23 +45,50 @@ fn main() -> Result<()> {
         return result.iter().fold(0, |acc, e| acc + e);
     });
 
-    let mut problem: Array2<char> = Array2::default((0, problem_len));
+    let mut cur_prob: Vec<i64> = Vec::new();
+    let mut problems: Vec<Vec<i64>> = Vec::new();
 
-    for line in lines.lines() {
-        if line.contains("+") || line.contains("*") {
+    for x in (0..problem_len).rev() {
+        let mut acc: String = String::new();
+        for y in 0..inputs.len() {
+            acc.push_str(inputs[y][x].to_string().as_str());
+        }
+
+        acc = acc.trim().to_string();
+
+        if acc == "" {
+            problems.push(cur_prob.clone());
+            cur_prob = Vec::new();
             continue;
         }
 
-        let chars: Vec<char> = line.chars().collect();
-        problem.append(
-            Axis(0),
-            Array::from_shape_vec((1, line.len()), chars)
-                .unwrap()
-                .view(),
-        )?;
+        if acc.chars().last().unwrap() == '+' || acc.chars().last().unwrap() == '*' {
+            acc = acc[..acc.len() - 1].to_string();
+        }
+
+        cur_prob.push(acc.trim().parse::<i64>().unwrap());
     }
 
-    println!("{}", problem);
+    problems.push(cur_prob.clone());
+
+    cli::stage(2, || -> i64 {
+        let mut new_operators = operators.clone();
+        new_operators.reverse();
+        let mut result: Vec<i64> = Vec::new();
+
+        for i in 0..problems.len() {
+            let res =
+                problems[i][1..]
+                    .iter()
+                    .fold(problems[i][0], |acc, e| match new_operators[i] {
+                        Operator::Addition => acc + e,
+                        Operator::Multiplication => acc * e,
+                    });
+            result.push(res);
+        }
+
+        return result.iter().fold(0, |acc, e| acc + e);
+    });
 
     Ok(())
 }
